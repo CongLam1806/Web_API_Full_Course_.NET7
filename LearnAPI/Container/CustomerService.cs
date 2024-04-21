5cac6d0a73e49f06a9622a5c44d8feae884679b3
CongLam1806 <lamtc1806@gmail.com>
2024-04-21 23:15:13 +0700

    Add project files.

---
+++ LearnAPI/Container/CustomerService.cs
@@ -0,0 +1,22 @@
+﻿using LearnAPI.Modal;
+using LearnAPI.Repos;
+using LearnAPI.Repos.Models;
+using LearnAPI.Service;
+
+namespace LearnAPI.Container
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly LearndataContext context;
+        public CustomerService(LearndataContext context) 
+        {
+            this.context = context;
+        }
+        public List<CustomerModal> GetAll()
+        {
+            List<CustomerModal> _response = new List<CustomerModal>();
+            var _data = this.context.TblCustomers.ToList();
+            return _response;
+        }
+    }
+}
---
+++ LearnAPI/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using LearnAPI.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService service;
+        public CustomerController(ICustomerService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = this.service.GetAll();
+            if(data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+    }
+}
